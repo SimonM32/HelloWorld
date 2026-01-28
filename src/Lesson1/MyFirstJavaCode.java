12f5103035ae1ca64785635d05db748a7a951ebb
SimonM32 <simi1002@stud.ek.dk>
2026-01-28 18:57:38 +0100

    First Commit

---
+++ src/Lesson1/MyFirstJavaCode.java
@@ -0,0 +1,14 @@
+package Lesson1;
+
+public class MyFirstJavaCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! This is my first Java Code! Jubbi!");
+    }
+
+}
+
+
+
+
+
